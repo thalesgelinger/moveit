c5e5dd58dfa729580d6283cf04f0c343d892b11f
thalesgelinger <thalesgelinger@gmail.com>
2021-02-23 10:09:51 -0300

    feat: add day 1

---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { ExperienceBar } from "./ExperienceBar";
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { ExperienceBar } from "../components";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
